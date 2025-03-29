4cb4d73035f8eca83a2d7b6367de616df2541a75
Valentine Schmidt <vboo@Valentines-Mac.local>
2025-03-29 10:34:45 -0400

    tokyonight storm & git blame setup

--- init.lua
+++ init.lua
@@ -286,6 +286,7 @@ require('lazy').setup({
         topdelete = { text = '‾' }, ---@diagnostic disable-line: missing-fields
         changedelete = { text = '~' }, ---@diagnostic disable-line: missing-fields
       },
+      current_line_blame = true,
     },
   },
 
@@ -818,12 +819,13 @@ require('lazy').setup({
         styles = {
           comments = { italic = false }, -- Disable italics in comments
         },
+        transparent = true, -- Sets transparent neovim background
       }
 
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-storm'
     end,
   },
 
